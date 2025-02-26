0d83c9765311f9f4b1097c84c53ed1e6edf73a29
Shalini <shalini@Shalinis-MacBook-Pro.local>
2025-02-26 21:28:02 +0530

    Removed unwanted code

--- handler/handlers.go
+++ handler/handlers.go
@@ -2,15 +2,11 @@ package handler
 
 import (
 	"coffeeshop/coffee"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-//	func Ping(w http.ResponseWriter, r *http.Request) {
-//		fmt.Println("Got a ping")
-//		io.WriteString(w, "Welcome to the Coffeeshop!\n")
 func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Welcome to the Coffeeshop!",
@@ -30,16 +26,4 @@ func HTMLCoffeeDetails(c *gin.Context) {
 func CoffeeDetails(c *gin.Context) {
 	coffeelist, _ := coffee.GetCoffees()
 	c.String(http.StatusOK, " %s", coffeelist)
-	fmt.Println("Printing the list of coffees available in the CoffeeShop ")
-	// coffees, err := coffee.GetCoffees()
-	// if err != nil {
-	// 	fmt.Println("Error while getting coffeelist ", err)
-	// 	return
-	// }
-
-	// for _, element := range coffees.List {
-	// 	result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
-	// 	fmt.Println(result)
-	// }
-	// fmt.Println("Is Latte Available? ", coffee.IsCoffeeAvailable("Latte"))
 }
--- main.go
+++ main.go
@@ -8,22 +8,11 @@ import (
 )
 
 func main() {
-
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
-	// r.GET("/home", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.html", nil)
-	// })
 	r.GET("/home", handler.HTMLCoffeeDetails)
 	r.GET("/ping", handler.Ping)
 	r.GET("/coffee", handler.CoffeeDetails)
 	fmt.Println("Starting the server at port 8081")
 	r.Run(":8081")
-
-	// http.HandleFunc("/ping", handler.Ping)
-	// fmt.Println("Starting the server at port 8081")
-	// err := http.ListenAndServe(":8081", nil)
-	// if err != nil {
-	// 	fmt.Println("Error while starting the server ", err)
-	// }
 }
